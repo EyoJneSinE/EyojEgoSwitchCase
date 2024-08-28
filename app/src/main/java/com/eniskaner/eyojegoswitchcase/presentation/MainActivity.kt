cc77d92cbc2fdb99c21dd5471f2c87bbaa945021
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-29 02:02:12 +0300

    create: main switches package

--- app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/MainActivity.kt
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.eniskaner.eyojegoswitchcase
+package com.eniskaner.eyojegoswitchcase.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.eniskaner.eyojegoswitchcase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
