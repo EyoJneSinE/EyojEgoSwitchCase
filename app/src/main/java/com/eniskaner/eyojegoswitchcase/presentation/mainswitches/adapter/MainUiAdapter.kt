c14d8fcad4b8aef3c725235be36497e7140d825c
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-29 16:02:41 +0300

    bind: MainUiAdapter with MainUiViewHolder

--- app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/mainswitches/adapter/MainUiAdapter.kt
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/mainswitches/adapter/MainUiAdapter.kt
@@ -1,16 +1,19 @@
 package com.eniskaner.eyojegoswitchcase.presentation.mainswitches.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.eniskaner.eyojegoswitchcase.databinding.ItemSwitchBinding
 import com.eniskaner.eyojegoswitchcase.presentation.mainswitches.model.SwitchPreferencesUIModel
 import com.eniskaner.eyojegoswitchcase.presentation.mainswitches.viewholder.MainUiViewHolder
 
 class MainUiAdapter(): ListAdapter<SwitchPreferencesUIModel, MainUiViewHolder>(MainSwitchesDiffCallBack()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainUiViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemSwitchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return  MainUiViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MainUiViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindSwitchListToRecyclerView(getItem(position))
     }
 }
