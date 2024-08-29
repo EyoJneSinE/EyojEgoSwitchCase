34c180e5cf9db0baeee9d38286af3464e3e8f845
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-29 21:52:50 +0300

    add: bottom navigation on main activity

--- app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/MainActivity.kt
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/MainActivity.kt
@@ -5,23 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.eniskaner.eyojegoswitchcase.R
 import com.eniskaner.eyojegoswitchcase.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_main_activity)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.masNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        setupBottomNavigationBar()
+
+    }
+
+    private fun setupBottomNavigationBar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.masNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val fragmentTitles = mapOf(
+            R.id.mainFragment to "main",
+            R.id.switch1Fragment to "a",
+            R.id.switch2Fragment to "b",
+            R.id.switch3Fragment to "c",
+            R.id.switch4Fragment to "d",
+            R.id.switch5Fragment to "e"
+        )
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.toolbar.title = fragmentTitles[navController.currentDestination?.id]
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            binding.toolbar.title = fragmentTitles[destination.id]
+            binding.toolbar.navigationIcon = null
         }
     }
 }
