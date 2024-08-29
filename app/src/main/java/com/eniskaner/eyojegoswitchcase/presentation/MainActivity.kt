8682ba425c9daeb69d08a752bf07f86305128c8a
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-29 20:22:36 +0300

    add: binding on MainActivity

--- app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/MainActivity.kt
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.eniskaner.eyojegoswitchcase.R
+import com.eniskaner.eyojegoswitchcase.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_main_activity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
