47f95b406515eae065365052250f6c276a64336d
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-29 16:20:40 +0300

    add: SwitchPreferencesUIModelProvider for switches data provide

---
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/data/datamodelprovider/SwitchPreferencesUIModelProvider.kt
@@ -0,0 +1,43 @@
+package com.eniskaner.eyojegoswitchcase.data.datamodelprovider
+
+import com.eniskaner.eyojegoswitchcase.presentation.mainswitches.model.SwitchPreferencesUIModel
+import com.eniskaner.eyojegoswitchcase.presentation.mainswitches.model.SwitchType
+import javax.inject.Inject
+
+class SwitchPreferencesUIModelProvider @Inject constructor() {
+
+    private fun addSwitches(): List<SwitchPreferencesUIModel> {
+        val switchList = listOf(SwitchPreferencesUIModel(
+            isEnabled = false,
+            isChecked = false,
+            switchName = "a",
+            switchType = SwitchType.OTHERS
+        ),  SwitchPreferencesUIModel(
+            isEnabled = false,
+            isChecked = false,
+            switchName = "b",
+            switchType = SwitchType.OTHERS
+        ), SwitchPreferencesUIModel(
+            isEnabled = false,
+            isChecked = false,
+            switchName = "c",
+            switchType = SwitchType.OTHERS
+        ), SwitchPreferencesUIModel(
+            isEnabled = false,
+            isChecked = false,
+            switchName = "d",
+            switchType = SwitchType.OTHERS
+        ), SwitchPreferencesUIModel(
+            isEnabled = false,
+            isChecked = false,
+            switchName = "e",
+            switchType = SwitchType.OTHERS
+        ), SwitchPreferencesUIModel(
+            isEnabled = true,
+            isChecked = true,
+            switchName = "Ego",
+            switchType = SwitchType.EGO
+        ))
+        return  switchList
+    }
+}
