7e0ce8266a093e75e8cc9e564470471c9cdafc8c
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-29 15:55:53 +0300

    create: MainSwitchesDiffCallBack for MainAdapter

---
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/mainswitches/adapter/MainSwitchesDiffCallBack.kt
@@ -0,0 +1,20 @@
+package com.eniskaner.eyojegoswitchcase.presentation.mainswitches.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.eniskaner.eyojegoswitchcase.presentation.mainswitches.model.SwitchPreferencesUIModel
+
+class MainSwitchesDiffCallBack: DiffUtil.ItemCallback<SwitchPreferencesUIModel>() {
+    override fun areItemsTheSame(
+        oldItem: SwitchPreferencesUIModel,
+        newItem: SwitchPreferencesUIModel
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(
+        oldItem: SwitchPreferencesUIModel,
+        newItem: SwitchPreferencesUIModel
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+}
