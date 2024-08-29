aeecccc1dde337513e80e1a362c2366bf6c9148d
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-29 16:04:35 +0300

    create: MainFragmentViewModel

---
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/mainswitches/viewmodel/MainFragmentViewModel.kt
@@ -0,0 +1,8 @@
+package com.eniskaner.eyojegoswitchcase.presentation.mainswitches.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainFragmentViewModel: ViewModel() {
+}
