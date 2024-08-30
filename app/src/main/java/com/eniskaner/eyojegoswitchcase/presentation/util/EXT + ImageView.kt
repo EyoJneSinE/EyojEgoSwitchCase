0d1fb66f058139f74ef8e9500d2853bbe090241f
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-30 19:14:38 +0300

    change: Glide ext for different images

--- app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/util/EXT + ImageView.kt
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/util/EXT + ImageView.kt
@@ -2,9 +2,26 @@ package com.eniskaner.eyojegoswitchcase.presentation.util
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 fun ImageView.load(url: String) {
     Glide.with(this.context)
         .load(url)
+        .into(this)
+}
+
+fun ImageView.loadCircleCrop(url: String) {
+    Glide.with(this.context)
+        .load(url)
+        .circleCrop()
+        .into(this)
+}
+
+fun ImageView.loadCenterCrop(url: String) {
+    Glide.with(this.context)
+        .load(url)
+        .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(80)))
         .into(this)
 }
