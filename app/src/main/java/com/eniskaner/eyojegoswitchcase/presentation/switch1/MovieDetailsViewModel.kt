6d68cdf7ec41c1afdc45f09df0866f5bf09b6c90
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-30 19:17:16 +0300

    add: cast list func

--- app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/switch1/MovieDetailsViewModel.kt
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/switch1/MovieDetailsViewModel.kt
@@ -1,9 +1,7 @@
 package com.eniskaner.eyojegoswitchcase.presentation.switch1
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.eniskaner.eyojegoswitchcase.data.remote.util.Constants.MOVIE_ID
 import com.eniskaner.eyojegoswitchcase.domain.movies_usecase.GetMovieDetailsUseCase
 import com.eniskaner.eyojegoswitchcase.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,24 +14,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MovieDetailsViewModel @Inject constructor(
-    private val getMovieDetailsUseCase: GetMovieDetailsUseCase,
-    private val savedMovieDetailsStateHandle: SavedStateHandle,
+    private val getMovieDetailsUseCase: GetMovieDetailsUseCase
 ) : ViewModel() {
 
-    private val _stateMovieDetails = MutableStateFlow<MovieDetailState>(MovieDetailState())
+    private val _stateMovieDetails = MutableStateFlow(MovieDetailState())
     val stateMovieDetails : StateFlow<MovieDetailState> = _stateMovieDetails
 
     private var jobMovieDetails : Job? = null
 
-    init {
-        savedMovieDetailsStateHandle.get<String>(MOVIE_ID.toString())?.let {
-            getMovieDetails(it.toInt())
-        }
-    }
-
     fun getMovieDetails(imdbId: Int) {
         jobMovieDetails?.cancel()
-        jobMovieDetails = getMovieDetailsUseCase.executeGetMovieDetailsFromTMDB(imdbId).onEach {
+        jobMovieDetails = getMovieDetailsUseCase.invoke(movieId = imdbId).onEach {
             when (it) {
                 is Resource.Success -> {
                     _stateMovieDetails.value = MovieDetailState(movieDetails = it.data)
--- app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/switch1/MoviesCastViewModel.kt
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/switch1/MoviesCastViewModel.kt
@@ -1,10 +1,9 @@
 package com.eniskaner.eyojegoswitchcase.presentation.switch1
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.eniskaner.eyojegoswitchcase.data.remote.util.Constants.MOVIE_ID
-import com.eniskaner.eyojegoswitchcase.domain.movies_usecase.GetMovieDetailsCastAndCrewUseCase
+import com.eniskaner.eyojegoswitchcase.domain.movies_usecase.GetMovieDetailCastListUseCase
+import com.eniskaner.eyojegoswitchcase.domain.movies_usecase.GetMovieDetailCastUseCase
 import com.eniskaner.eyojegoswitchcase.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
@@ -16,27 +15,39 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MoviesCastViewModel @Inject constructor(
-    private val getMovieDetailsCastUseCase: GetMovieDetailsCastAndCrewUseCase,
-    private val savedMovieCastStateHandle: SavedStateHandle
+    private val getMovieDetailsCastUseCase: GetMovieDetailCastUseCase,
+    private val getMovieDetailCastListUseCase: GetMovieDetailCastListUseCase
 ): ViewModel() {
 
-    private val _stateMovieCast = MutableStateFlow<MovieDetailState>(MovieDetailState())
+    private val _stateMovieCast = MutableStateFlow(MovieDetailState())
     val stateMovieCast : StateFlow<MovieDetailState> = _stateMovieCast
 
     private var jobMovieCasts : Job? = null
+    private var jobMovieCastList : Job? = null
 
-    init {
-        savedMovieCastStateHandle.get<String>(MOVIE_ID.toString())?.let {
-            getMovieCast(it.toInt())
-        }
+    fun getMovieCastList(imdbId: Int) {
+        jobMovieCastList?.cancel()
+        jobMovieCastList = getMovieDetailCastListUseCase.invoke(imdbId).onEach {
+            when (it) {
+                is Resource.Success -> {
+                    _stateMovieCast.value = MovieDetailState(movieCast = it.data?.movieCastList ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _stateMovieCast.value = MovieDetailState(error = it.message ?: "Error!")
+                }
+                is Resource.Loading -> {
+                    _stateMovieCast.value = MovieDetailState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 
     fun getMovieCast(imdbId: Int) {
         jobMovieCasts?.cancel()
-        jobMovieCasts = getMovieDetailsCastUseCase.executeGetCastAndCrewFromTMDB(imdbId).onEach {
+        jobMovieCasts = getMovieDetailsCastUseCase.invoke(imdbId).onEach {
             when (it) {
                 is Resource.Success -> {
-                    _stateMovieCast.value = MovieDetailState(movieCast = it.data)
+                    _stateMovieCast.value = MovieDetailState(movieCast = it.data ?: emptyList())
                 }
                 is Resource.Error -> {
                     _stateMovieCast.value = MovieDetailState(error = it.message ?: "Error!")
