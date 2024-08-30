f20177aadb4b19c932958af0b0c316494405d9e8
Enis Kaner <enis.d.kaner@gmail.com>
2024-08-30 03:16:02 +0300

    add: MovieDetailsDisplayItem for type

---
+++ app/src/main/java/com/eniskaner/eyojegoswitchcase/presentation/switch1/MovieDetailsDisplayItem.kt
@@ -0,0 +1,14 @@
+package com.eniskaner.eyojegoswitchcase.presentation.switch1
+
+interface MovieDetailsDisplayItem {
+
+    fun type(): Int
+
+    companion object {
+        const val TYPE_MOVIE_DETAILS = 0
+        const val TYPE_MOVIE_CAST_LIST = 1
+        const val TYPE_MOVIE_CREW_LIST = 2
+        const val TYPE_MOVIE_CAST = 3
+        const val TYPE_MOVIE_CREW = 4
+    }
+}
